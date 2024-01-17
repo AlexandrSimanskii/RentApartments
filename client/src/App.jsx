d5965a4b957a55ac3f9221c884422a14439cd947
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2024-01-17 17:29:20 +0200

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import "./assets/scss/main.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signOut" element={<SignOut />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-inner container">
+        <Link to={"/"}>
+          <h1 className="header-title">
+            <span className="header-title__first">Sahand</span>
+            <span className="header-title__second">Estate</span>
+          </h1>
+        </Link>
+
+        <form className="header-form">
+          <input className="header-search" type="text" placeholder="Поиск..." />
+          <FaSearch />
+        </form>
+        <ul className="header-list">
+          <Link to={"/"}>
+            <li className="header-list__item">Главная</li>
+          </Link>
+          <Link to={"/about"}>
+            <li className="header-list__item">О нас</li>
+          </Link>
+          <Link to={"/signIn"}>
+            <li className="header-list__item">Войти</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
