45f8d023beb2a5e12c56480da7bde3df26a90dbc
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2024-01-18 11:19:22 +0200

    connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+console.log(dotenv.config.MONGO);
+mongoose
+  .connect(process.env.MONGO)~
+  .then(() => {
+    console.log("Connect with MongoDB");
+  })
+  .catch((err) => console.log("Не удалось подключиться к MongoDB", err));
 
 const api = express();
 
