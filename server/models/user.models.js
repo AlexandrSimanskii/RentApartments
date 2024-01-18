952ba0fe9043ab856ea33bd6b3bfaf92b698865f
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2024-01-18 13:37:27 +0200

    Create user model

---
+++ server/models/user.models.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const userScheme = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userScheme);
+export default User;
