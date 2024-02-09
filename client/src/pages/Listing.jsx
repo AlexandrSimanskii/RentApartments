c934640e98f8262bc543910241704365f10e67b3
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2024-02-09 16:33:42 +0200

    Complete create listing page functionality

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -6,16 +6,32 @@ import {
 } from "firebase/storage";
 import { useState } from "react";
 import { app } from "../firebase";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Listing = () => {
   const [files, setFiles] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [imageUploadError, setImageUploadError] = useState(false);
   const [uploading, setUploading] = useState(false);
+
   const [formData, setFormData] = useState({
     imageUrls: [],
+    name: "",
+    description: "",
+    address: "",
+    type: "Аренда",
+    bedrooms: 1,
+    bathrooms: 1,
+    regularPrice: 500,
+    discountPrice: 0,
+    offer: false,
+    parking: false,
+    furnished: false,
   });
-
-  console.log(uploading);
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
 
   const handleFileSubmit = () => {
     if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
@@ -81,10 +97,69 @@ const Listing = () => {
     }));
   };
 
+  const handleChange = (e) => {
+    if (e.target.id === "Продажа" || e.target.id === "Аренда") {
+      setFormData((prev) => ({
+        ...prev,
+        type: e.target.id,
+      }));
+    }
+
+    if (
+      e.target.id === "parking" ||
+      e.target.id === "furnished" ||
+      e.target.id === "offer"
+    ) {
+      setFormData((prev) => ({ ...prev, [e.target.id]: e.target.checked }));
+    }
+
+    if (
+      e.target.type === "number" ||
+      e.target.type === "text" ||
+      e.target.type === "textarea"
+    ) {
+      setFormData((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      if (formData.imageUrls.length < 1) {
+        return setError("Вы должны загрузить фотографию!");
+      }
+      if (formData.regularPrice < formData.discountPrice) {
+        return setError("Цена со скидкой не может быть больше цены!");
+      }
+
+      const res = await fetch("/api/listing/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ...formData, userRef: currentUser._id }),
+      });
+      const data = await res.json();
+
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+      }
+
+      navigate(`/listing/${data._id}`);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
+  console.log(formData);
   return (
     <section className="listing container">
       <h1 className="listing-title">Создать обьявление</h1>
-      <form className="listing-form" action="submit">
+      <form onSubmit={handleSubmit} className="listing-form" action="submit">
         <div className="listing-form__inner">
           <div className="add-data">
             <fieldset className="group-text">
@@ -93,35 +168,71 @@ const Listing = () => {
                 id="name"
                 required
                 placeholder="Вид недвижемости"
+                onChange={handleChange}
+                value={formData.name}
               />
               <textarea
                 id="description"
                 required
                 placeholder="Описание"
+                onChange={handleChange}
+                value={formData.description}
               ></textarea>
-              <input type="text" id="address" required placeholder="Адресс" />
+              <input
+                type="text"
+                id="address"
+                required
+                placeholder="Адресс"
+                onChange={handleChange}
+                value={formData.address}
+              />
             </fieldset>
             <fieldset className="group-checkbox">
               <label className="listing-label">
-                <input type="checkbox" id="sale" />
+                <input
+                  type="checkbox"
+                  id="Продажа"
+                  onChange={handleChange}
+                  checked={formData.type === "Продажа"}
+                />
                 Продажа
               </label>
 
               <label className="listing-label">
-                <input type="checkbox" id="rent" />
+                <input
+                  type="checkbox"
+                  id="Аренда"
+                  onChange={handleChange}
+                  checked={formData.type === "Аренда"}
+                />
                 Аренда
               </label>
               <label className="listing-label">
-                <input type="checkbox" id="parking" />
+                <input
+                  type="checkbox"
+                  id="parking"
+                  onChange={handleChange}
+                  checked={formData.parking}
+                />
                 Парковка
               </label>
               <label className="listing-label">
-                <input type="checkbox" id="furnished" />
+                <input
+                  type="checkbox"
+                  id="furnished"
+                  onChange={handleChange}
+                  checked={formData.furnished}
+                />
                 Мебель
               </label>
               <label className="listing-label">
-                <input type="checkbox" id="offer" />
-                Готовность
+                <input
+                  type="checkbox"
+                  id="offer"
+                  onChange={handleChange}
+                  checked={formData.offer}
+                />
+                Заключение договора
               </label>
             </fieldset>
             <fieldset className="group-number">
@@ -133,17 +244,21 @@ const Listing = () => {
                   required
                   min="1"
                   max="10"
+                  onChange={handleChange}
+                  value={formData.bedrooms}
                 />
-                Кровати
+                Комнаты
               </label>
               <label className="listing-label">
                 <input
                   className="group-number__item"
-                  id="baths"
+                  id="bathrooms"
                   type="number"
                   required
                   min="1"
                   max="10"
+                  onChange={handleChange}
+                  value={formData.bathrooms}
                 />
                 Ванные
               </label>
@@ -153,24 +268,32 @@ const Listing = () => {
                   id="regularPrice"
                   type="number"
                   required
-                  min="1"
-                  max="10"
+                  min="500"
+                  max="100000000"
+                  onChange={handleChange}
+                  value={formData.regularPrice}
                 />
                 <p className="group-number__item-text">
                   Цена <span>(Руб/месяц)</span>
                 </p>
               </label>
-              <label className="listing-label">
-                <input
-                  className="group-number__item"
-                  id="discountPrice"
-                  type="number"
-                  required
-                  min="1"
-                  max="10"
-                />
-                Цена со скидкой
-              </label>
+              {formData.offer && (
+                <label className="listing-label">
+                  <input
+                    className="group-number__item"
+                    id="discountPrice"
+                    type="number"
+                    required
+                    min="0"
+                    max="100000000"
+                    onChange={handleChange}
+                    value={formData.discountPrice}
+                  />
+                  <p className="group-number__item-text">
+                    Цена со скидкой <span>(Руб/месяц)</span>
+                  </p>
+                </label>
+              )}
             </fieldset>
           </div>
           <div className="add-pictures">
@@ -218,9 +341,13 @@ const Listing = () => {
             ))}
 
             <p className="error">{imageUploadError && imageUploadError}</p>
-            <button className="listing-form__button">
-              Добавить обьявление
+            <button
+              className="listing-form__button"
+              disabled={loading || uploading}
+            >
+              {loading ? "Загрузка..." : "Добавить обьявление"}
             </button>
+            {error && <p className="error">{error}</p>}
           </div>
         </div>
       </form>
