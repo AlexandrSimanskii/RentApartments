e251d64e3014cf1882eff391cbc19681d5769f12
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2024-02-11 23:50:28 +0200

    add image slider to the listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,14 +1,73 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import SwiperCore from "swiper";
+// import { Navigation } from "swiper/modules";
+// import "swiper/css/boundle";
+import { register } from "swiper/element/bundle";
+register();
 
 const Listing = () => {
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const params = useParams();
 
-const params = useParams()
+  console.log(params.listingID);
 
-    return (
-        <div>
-            {`Обьявление ${params.listingID}`}
-        </div>
-    );
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+
+        const res = await fetch(`/api/listing/get/${params.listingID}`);
+        const data = await res.json();
+
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          console.log(data.message);
+          return;
+        }
+
+        setError(false);
+        setLoading(false);
+        setListing(data);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+        console.log(error);
+      }
+    };
+    fetchListing();
+  }, [params.listingID]);
+  console.log(listing?.imageUrls);
+  return (
+    <main>
+      {loading && <p>Загрузка</p>}
+      {error && <p>Что-то пошло не так</p>}
+
+      {listing && !loading && !error && (
+        <>
+          <h1>{listing.name}</h1>
+          <div>
+            <swiper-container navigation="true" loop="true">
+              {listing.imageUrls.map((url) => (
+                <swiper-slide key={url}>
+                  <div
+                    className="swiper-images"
+                    style={{ backgroundImage: `url(${url})` }}
+                  >
+                    
+                  </div>
+                </swiper-slide>
+              ))}
+            </swiper-container>
+          </div>
+        </>
+      )}
+    </main>
+  );
 };
 
 export default Listing;
