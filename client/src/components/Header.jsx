7b486936e110e929d3b4f90282e0fc9aa8109e72
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2024-02-16 18:08:15 +0200

    update header search form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,49 +1,85 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const { currentUser } = useSelector((state) => state.user);
 
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+
+    urlParams.set("searchTerm", searchTerm);
+
+    const searchQuery = urlParams.toString();
+    const paramsIterator = urlParams.entries();
+
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <header className="header">
-      <div className="header-inner container">
-        <Link to={"/"}>
-          <h1 className="header-title">
-            <span className="header-title__first">Sahand</span>
-            <span className="header-title__second">Estate</span>
-          </h1>
-        </Link>
-
-        <form className="header-form">
-          <input className="header-search" type="text" placeholder="Поиск..." />
-
-          <FaSearch />
-        </form>
-        <ul className="header-list">
+      <div className="container">
+        <div className="header-inner">
           <Link to={"/"}>
-            <li className="header-list__item">Главная</li>
-          </Link>
-          <Link to={"/about"}>
-            <li className="header-list__item">О нас</li>
+            <h1 className="header-title">
+              <span className="header-title__first">Sahand</span>
+              <span className="header-title__second">Estate</span>
+            </h1>
           </Link>
 
-          {currentUser ? (
-            <Link to={"/profile"}>
-              <li className="header-list__item">
-                <img
-                  className="header-img"
-                  src={currentUser.avatar}
-                  alt="profile"
-                />
-              </li>
+          <form className="header-form" onSubmit={handleSubmit}>
+            <input
+              className="header-search"
+              type="text"
+              placeholder="Поиск..."
+              value={searchTerm}
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+              }}
+            />
+            <button className="header-form__button">
+              <FaSearch />
+            </button>
+          </form>
+          <ul className="header-list">
+            <Link to={"/"}>
+              <li className="header-list__item">Главная</li>
             </Link>
-          ) : (
-            <Link to={"/sign-in"}>
-              <li className="header-list__item">Войти</li>
+            <Link to={"/about"}>
+              <li className="header-list__item">О нас</li>
             </Link>
-          )}
-        </ul>
+
+            {currentUser ? (
+              <Link to={"/profile"}>
+                <li className="header-list__item">
+                  <img
+                    className="header-img"
+                    src={currentUser.avatar}
+                    alt="profile"
+                  />
+                </li>
+              </Link>
+            ) : (
+              <Link to={"/sign-in"}>
+                <li className="header-list__item">Войти</li>
+              </Link>
+            )}
+          </ul>
+        </div>
       </div>
     </header>
   );
